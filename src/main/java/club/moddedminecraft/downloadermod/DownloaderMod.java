eca6fd35fd4b36b0cf085545e7f54cb3ea7b01ea
john01dav <john01dav@gmail.com>
2020-07-25 16:55:09 -0500

    It works now!

--- src/main/java/club/moddedminecraft/downloadermod/DownloaderMod.java
+++ src/main/java/club/moddedminecraft/downloadermod/DownloaderMod.java
@@ -6,24 +6,49 @@ import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
 import javax.swing.*;
+import java.io.*;
+import java.net.URL;
 import java.util.ArrayList;
 
 @Mod(modid = DownloaderMod.MODID, version = DownloaderMod.VERSION)
-public final class DownloaderMod
-{
+public final class DownloaderMod{
     public static final String MODID = "Downloader Mod by Modded Minecraft Club";
     public static final String VERSION = "1.0";
 
     @EventHandler
     public void init(FMLInitializationEvent event){
+        ArrayList<ModToDownload> modsToDownload = loadModsList();
+        boolean didDownloadAtLeastOneMod = false;
 
-        JOptionPane.showMessageDialog(null, "To deal with licensing restrictions, some mods need to be installed after the game launches. To ensure that these mods are properly loaded, your client will now close. Please restart it to continue.");
-        throw new RestartYourClientException();
+        for(ModToDownload modToDownload : modsToDownload){
+            if(modToDownload.downloadIfNeeded()){
+                didDownloadAtLeastOneMod = true;
+            }
+        }
+
+        if(didDownloadAtLeastOneMod){
+            JOptionPane.showMessageDialog(null, "To deal with licensing restrictions, some mods need to be installed after the game launches. To ensure that these mods are properly loaded, your client will now close. Please restart it to continue.");
+            throw new RestartYourClientException();
+        }
     }
 
     private ArrayList<ModToDownload> loadModsList(){
+        try(
+                FileReader fileReader = new FileReader("modstodownload.txt");
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
+        ){
+            ArrayList<ModToDownload> modToDownloadArrayList = new ArrayList<>();
 
-
+            String line;
+            while((line = bufferedReader.readLine()) != null){
+                String url = line.substring(0, line.indexOf(";"));
+                String path = line.substring(url.length()+1);
+                modToDownloadArrayList.add(new ModToDownload(new URL(url), new File(path)));
+            }
+            return modToDownloadArrayList;
+        }catch(IOException e){
+            throw new RuntimeException(e);
+        }
     }
 
 }
--- src/main/java/club/moddedminecraft/downloadermod/ModToDownload.java
+++ src/main/java/club/moddedminecraft/downloadermod/ModToDownload.java
@@ -1,7 +1,11 @@
 package club.moddedminecraft.downloadermod;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.net.URLConnection;
 
 final class ModToDownload{
     private final URL url;
@@ -13,7 +17,28 @@ final class ModToDownload{
     }
 
     public boolean downloadIfNeeded(){
-        return true;
+        if(targetPath.exists()){
+            System.out.println("The mod " + url + " / " + targetPath + " is already downloaded, so it will be skipped.");
+            return false;
+        }else{
+            System.out.println("Downloading mod " + url + " to " + targetPath + ".");
+            try{
+                URLConnection connection = url.openConnection();
+                try(
+                    InputStream inputStream = connection.getInputStream();
+                    FileOutputStream fileOutputStream = new FileOutputStream(targetPath);
+                ){
+                    byte[] buffer = new byte[1024];
+                    int read;
+                    while((read = inputStream.read(buffer)) >= 0){
+                        fileOutputStream.write(buffer, 0, read);
+                    }
+                }
+            }catch(IOException e){
+                throw new RuntimeException(e);
+            }
+            return true;
+        }
     }
 
 }
